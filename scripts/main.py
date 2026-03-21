094320cba5e6f30b6b3e10a22fc82ce16a729fd0
radhakrishnaa2995-png <radhakrishnaa2995@gmail.com>
2026-03-21 17:28:53 +0530

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -1,22 +1,33 @@
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+
+# Fix path for GitHub Actions
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(BASE_DIR)
 
 import json
 import random
 from generator import generate_book
 
+
 def load_themes():
-    with open("templates/themes.json") as f:
+    # Correct path for GitHub
+    themes_path = os.path.join(BASE_DIR, "..", "templates", "themes.json")
+    
+    with open(themes_path) as f:
         return json.load(f)
+
 
 def main():
     themes = load_themes()
+
+    print("Themes loaded:", themes)
 
     for i in range(10):
         theme = random.choice(themes)
         file = generate_book(theme, i)
         print(f"Generated: {file}")
+
 
 if __name__ == "__main__":
     main()
